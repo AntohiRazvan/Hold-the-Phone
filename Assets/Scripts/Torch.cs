c6a9b460c72091b013ac64952b15771cf1a50df5
AntohiRazvan <antohirazvan@gmail.com>
2020-04-29 19:51:55 +0300

    Created torch prefab

---
+++ Assets/Scripts/Torch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+
+public class Torch : MonoBehaviour
+{
+    [SerializeField]
+    float fadeTime = 300;
+    [SerializeField]
+    float finalIntensity = 0.1f;
+    
+    System.DateTime startTime;
+    Light2D light;
+    float startingIntensity;
+
+    void Start()
+    {
+        startTime = System.DateTime.UtcNow;
+        light = gameObject.GetComponent<Light2D>();
+        startingIntensity = light.intensity;
+
+        StartCoroutine( FadeOverSeconds() );
+    }
+
+    public IEnumerator FadeOverSeconds()
+    {
+        float elapsedTime = 0;
+        while ( elapsedTime < fadeTime )
+        {
+            light.intensity = Mathf.Lerp( startingIntensity, finalIntensity, ( elapsedTime / fadeTime ));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        light.intensity = finalIntensity;
+ }
+}
