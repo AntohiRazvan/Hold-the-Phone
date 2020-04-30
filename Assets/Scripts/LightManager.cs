ac636794901fe09b8012a8898e6089cfcb82988e
AntohiRazvan <antohirazvan@gmail.com>
2020-04-30 23:34:03 +0300

    Created light manager

---
+++ Assets/Scripts/LightManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightManager : MonoBehaviour
+{
+    public static float lightIntensity;
+    public float fadeTime;
+    public float finalIntensity;
+    public float secondsLostPerHit;
+    
+    float startingIntensity;
+    float elapsedTime;
+    System.DateTime startTime;
+
+    void Awake()
+    {
+        GameEventManager.TookDamage += OnTookDamage;
+        DontDestroyOnLoad(this);
+    }
+
+    void Start()
+    {
+        startTime = System.DateTime.UtcNow;
+        lightIntensity = 1f;
+        startingIntensity = lightIntensity;
+        elapsedTime = 0;
+    }
+
+    void Update()
+    {
+        Debug.Log(lightIntensity);
+        if( elapsedTime <= fadeTime )
+        {
+            lightIntensity = Mathf.Lerp( startingIntensity, finalIntensity, ( elapsedTime / fadeTime ));
+            elapsedTime += Time.deltaTime;
+        }
+        else
+        {
+            GameEventManager.TriggerGameOver();
+        }      
+    }
+
+    void OnTookDamage()
+    {
+        fadeTime -= secondsLostPerHit;
+    }
+}
--- Assets/Scripts/Torch.cs
+++ Assets/Scripts/Torch.cs
@@ -6,33 +6,15 @@ using UnityEngine.Experimental.Rendering.Universal;
 
 public class Torch : MonoBehaviour
 {
-    [SerializeField]
-    float fadeTime = 300;
-    [SerializeField]
-    float finalIntensity = 0.1f;
-    
-    System.DateTime startTime;
     Light2D light;
-    float startingIntensity;
 
     void Awake()
     {
-        startTime = System.DateTime.UtcNow;
         light = gameObject.GetComponent<Light2D>();
-        startingIntensity = light.intensity;
-
-        StartCoroutine( FadeOverSeconds() );
     }
 
-    public IEnumerator FadeOverSeconds()
+    void Update()
     {
-        float elapsedTime = 0;
-        while ( elapsedTime < fadeTime )
-        {
-            light.intensity = Mathf.Lerp( startingIntensity, finalIntensity, ( elapsedTime / fadeTime ));
-            elapsedTime += Time.deltaTime;
-            yield return new WaitForEndOfFrame();
-        }
-        light.intensity = finalIntensity;
- }
+        light.intensity = LightManager.lightIntensity;
+    }
 }
