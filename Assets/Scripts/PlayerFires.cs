7586657e86287738ff200124887585035f1faa50
Razvan Rotari <razvanrotari@gmail.com>
2020-04-30 12:30:20 +0300

    Added shooting for player

---
+++ Assets/Scripts/PlayerFires.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFires : MonoBehaviour
+{
+	public int fireSpeed;
+	public GameObject projectile;
+	Transform firePoint;
+	// Start is called before the first frame update
+	void Start()
+	{
+
+		firePoint = transform.Find("FirePoint") ;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetButtonDown("Fire1")) {
+             GameObject bullet = Instantiate(projectile, firePoint.transform.position, Quaternion.identity) as GameObject;
+             bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.localPosition.normalized * 600);
+		}
+	}
+
+
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,8 +8,9 @@ public class PlayerMovement : MonoBehaviour
 	float baseSpeed = 4f;
 	float movementSpeed;
 
-	Vector2 movementDirection;         
+	Vector2 movementDirection;
 	Rigidbody2D rigidbody;
+	Transform firePoint;
 	public Animator anim;
 
 
@@ -17,13 +18,14 @@ public class PlayerMovement : MonoBehaviour
 	{
 		rigidbody = this.GetComponent<Rigidbody2D>();
 		anim = this.GetComponentInChildren<Animator>();
+		firePoint = transform.Find("FirePoint") ;
 	}
 
 	void FixedUpdate()
 	{
 
 		rigidbody.MovePosition(rigidbody.position + movementDirection * movementSpeed * baseSpeed * Time.fixedDeltaTime);
-	
+
 		if(movementDirection.sqrMagnitude - 0.01f >= 0f)
 		{
 			anim.SetFloat("Horizontal", movementDirection.x);
@@ -37,5 +39,12 @@ public class PlayerMovement : MonoBehaviour
 		movementDirection = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 		movementSpeed = Mathf.Clamp(movementDirection.sqrMagnitude, 0f, 1f);
 		movementDirection.Normalize();
+		if (movementDirection.y > 0) {
+			firePoint.localPosition  = new Vector3(0f, 0.25f, 0f);
+		} else if (movementDirection.y < 0) {
+			firePoint.localPosition  = new Vector3(0f, -0.75f, 0f);
+		} else if (movementDirection.x != 0) {
+			firePoint.localPosition  = new Vector3(0.25f * Mathf.Sign(movementDirection.x), 0f, 0f);
+		}
 	}
 }
