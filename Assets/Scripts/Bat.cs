d8eca3d19ad47004eb5cb57ba73365e8785a78f7
AntohiRazvan <antohirazvan@gmail.com>
2020-04-30 18:45:34 +0300

    Bat now follows you around

--- Assets/Scripts/Bat.cs
+++ Assets/Scripts/Bat.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class Bat : MonoBehaviour
 {
-    public Transform goal;
+    Transform goal;
 
-    void Start()
+    UnityEngine.AI.NavMeshAgent agent;
+    Animator anim;
+
+    void Awake()
     {
-        UnityEngine.AI.NavMeshAgent agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
-        agent.SetDestination(goal.position);
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void Start() 
+    {
+        goal = GameObject.FindGameObjectsWithTag("Player")[0].transform;
     }
 
     void Update()
     {
-        
+        Vector2 movementDirection = Vector2.zero;
+        agent.SetDestination(goal.position);
+        if(movementDirection.sqrMagnitude - 0.01f >= 0f)
+		{
+            movementDirection = (goal.position - gameObject.transform.position).normalized;
+			anim.SetFloat("Horizontal", movementDirection.x);
+			anim.SetFloat("Vertical", movementDirection.y);
+		}
     }
 }
