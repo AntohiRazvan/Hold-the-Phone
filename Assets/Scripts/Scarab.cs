26b6875e2f3a3a471f463ceb6c4cba3c45f5b7a8
Razvan Rotari <razvanrotari@gmail.com>
2020-05-01 20:34:24 +0300

    Baby scarab

--- Assets/Scripts/Scarab.cs
+++ Assets/Scripts/Scarab.cs
@@ -6,16 +6,25 @@ public class Scarab : MonoBehaviour
 {
     public float damage;
     public float damageFrequency;
+	public bool baby;
+	public bool hasBabies;
+	public GameObject babyPrefab;
+	public GameObject[] spawns = new GameObject[3];
+	public Transform sprite;
     Transform goal;
     
     UnityEngine.AI.NavMeshAgent agent;
     Animator anim;
+	Health health;
+
     float lastDamage;
 
     void Awake()
     {
         agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
         anim = GetComponentInChildren<Animator>();
+		health = GetComponent<Health>();
+		sprite = transform.GetChild(0);
     }
 
     void Start() 
@@ -27,24 +36,37 @@ public class Scarab : MonoBehaviour
 	public GameObject dest; 
     void Update()
     {
-        if(agent.enabled)
+        if(agent.enabled && !baby)
         {
             Vector2 movementDirection = Vector2.zero;
             agent.SetDestination(goal.position);
             movementDirection = (goal.position - gameObject.transform.position).normalized;
 			anim.SetFloat("Speed", movementDirection.sqrMagnitude);
-			Vector3 rotation = transform.localEulerAngles;
+			Vector3 rotation = Vector3.zero;
 			if (Mathf.Abs(movementDirection.y) - 0.01f >= 0f) {
 				rotation.z = Mathf.Sign(movementDirection.y) * 90;
 			} else if (Mathf.Abs(movementDirection.x) - 0.01f >= 0f) {
 				rotation.z = Mathf.Sign(movementDirection.x) * 180;
 			}
-			/* transform.localEulerAngles = rotation; */
+			rotation.x = 90;
+			sprite.localEulerAngles = rotation;
 
         } else {
 			/* transform.position = Vector3.MoveTowards(transform.position, dest.transform.position, Time.deltaTime); */
 		}
     }
+
+	void FixedUpdate() {
+		if (health.health <=0 && !baby && !hasBabies) {
+			hasBabies = true;
+			spawnBabies();
+		}
+	}
+	void spawnBabies() {
+		foreach ( GameObject spawn in spawns) {
+			GameObject baby = Instantiate(babyPrefab, spawn.transform.position, Quaternion.identity) as GameObject;
+		} 
+	}
 
     void OnTriggerStay2D(Collider2D collider)
     {
